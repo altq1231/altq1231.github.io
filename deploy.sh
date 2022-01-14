79c40d14918ffdbfe9d2cbb903cc0b3465772e73
zhong.li@D1301004996 <zhong.li@united-imaging.com>
2022-01-14 11:07:35 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,4 @@
-# 确保脚本抛出遇到的错误
-set -e
-
-# 生成静态文件
-npm run build
-
-cd dist
-
-git init
-git add -A
-git commit -m 'deploy'
-
-git push -f git@github.com:AltQ1231/altq1231.github.io.git master
-
-cd -
-rm -rf dist
+git add .
+read commitString
+git commit -m commitString
+git push
